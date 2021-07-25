f9fef0fb17f1f4ce82459e52e444a50d79261064
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2021-07-25 22:10:54 +0200

    Added Zalando Money Jackson Serializer Library

--- backend/src/main/java/com/open/lms/config/JacksonConfig.java
+++ backend/src/main/java/com/open/lms/config/JacksonConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.jackson.datatype.money.MoneyModule;
 
 
 @Configuration
@@ -12,7 +13,9 @@ public class JacksonConfig {
 
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jacksonCustomizer() {
-        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.featuresToDisable(
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder
+                .modulesToInstall(new MoneyModule())
+                .featuresToDisable(
                 DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,
                 DeserializationFeature.ACCEPT_FLOAT_AS_INT,
                 SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
